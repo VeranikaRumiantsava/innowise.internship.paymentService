810a22f14d927143bfd4fdb357a32db2c9f8f774
rumronya <rum.ronya@gmail.com>
2025-10-07 10:23:06 +0300

    docker

---
+++ src/main/java/org/innowise/internship/payment_service/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package org.innowise.internship.payment_service.controllers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
+
