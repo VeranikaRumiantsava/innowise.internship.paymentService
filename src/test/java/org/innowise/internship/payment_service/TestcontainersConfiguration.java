4b214d8d0b792ef72d6bb114fe3367d279dc28ed
rumronya <rum.ronya@gmail.com>
2025-10-08 12:01:02 +0300

    try w\ tests 0.3

--- src/test/java/org/innowise/internship/payment_service/TestcontainersConfiguration.java
+++ src/test/java/org/innowise/internship/payment_service/TestcontainersConfiguration.java
@@ -9,17 +9,17 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-	@Bean
-	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
-	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
+//
+//	@Bean
+//	@ServiceConnection
+//	KafkaContainer kafkaContainer() {
+//		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+//	}
+//
+//	@Bean
+//	@ServiceConnection
+//	MongoDBContainer mongoDbContainer() {
+//		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+//	}
 
 }
--- src/test/java/org/innowise/internship/payment_service/controllers/BaseIT.java
+++ src/test/java/org/innowise/internship/payment_service/controllers/BaseIT.java
@@ -44,11 +44,11 @@ public abstract class BaseIT {
     protected PaymentKafkaProducer paymentKafkaProducer;
 
     @Container
-    @ServiceConnection(name = "mongo")
+    //@ServiceConnection(name = "mongo")
     public static final MongoDBContainer mongoContainer = new MongoDBContainer("mongo:6.0.8");
 
     @Container
-    @ServiceConnection(name = "kafka")
+    //@ServiceConnection(name = "kafka")
     public static final KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.7.5"));
 
     public static final WireMockServer  WIREMOCK = new WireMockServer(WireMockConfiguration.options().dynamicPort());
